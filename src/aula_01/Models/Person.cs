fec18e1a6f2ea5557568c65258804ef9965e8b9b
= <=>
2024-05-24 16:56:44 -0300

    chore(): renomeia a pasta para seguir padrão Pascal Case.

---
+++ src/aula_01/Models/Person.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula_01.Models;
+
+public class Person
+{
+    public string Name { get; set; }   
+    public int Age { get; set; }
+
+    public void Show(){
+        Console.WriteLine($"Nome: {Name} | Idade: {Age}");
+    }
+}
--- src/aula_01/Program.cs
+++ src/aula_01/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Aula_01.Models;
+
 Console.WriteLine("Hello, World!");
+
+var person = new Person(){Name="Narto",Age=33};
+person.Show();
